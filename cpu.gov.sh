d3bd1d0b4510d7a85e69ef9d676671b1541a48e7
DavidM42 <DavidMerz@gmx.de>
2017-02-11 20:21:19 +0100

    changed permissions, repaired install and uninstall

--- cpu.gov.sh
+++ cpu.gov.sh
@@ -19,7 +19,7 @@ function governor_changer {
 	  named='$named'
 	  time='$time'
 	  syslog='$syslog'
-	  echo -e "#!/bin/sh\n### BEGIN INIT INFO\n# Provides:          "$initd_file"\n# Required-Start:    $local_fs $network $named $time $syslog\n# Required-Stop:     $local_fs $network $named $time $syslog\n# Default-Start:     2 3 4 5\n# Default-Stop:      0 1 6\n# Description:       script to automatically set cpu governors and ondemand threshold to desired value at boot\n### END INIT INFO\n\nsudo echo $1 | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor" | sudo tee /etc/init.d/cpu_gov.sh > /dev/null 2>&1
+	  echo -e "#!/bin/sh\n### BEGIN INIT INFO\n# Provides:          "$initd_file"\n# Required-Start:    $local_fs $network $named $time $syslog\n# Required-Stop:     $local_fs $network $named $time $syslog\n# Default-Start:     2 3 4 5\n# Default-Stop:      0 1 6\n# Description:       script to automatically set cpu governors and ondemand threshold to desired value at boot\n### END INIT INFO\n\nsudo echo $1 | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor" | sudo tee /etc/init.d/cpu_gov > /dev/null 2>&1
 	  echo "Success: Set governor to $RESULT"
     else
       echo "Failed. You probably don't have sudo rights"
@@ -91,7 +91,7 @@ function change_up_threshold {
 		governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 		if [[ "$governor" == "ondemand" ]] || [[ "$governor" == "conservative" ]]; then
 			echo $1 | sudo tee /sys/devices/system/cpu/cpufreq/"$governor"/up_threshold > /dev/null 2>&1
-			echo "echo $1| sudo tee /sys/devices/system/cpu/cpufreq/$governor/up_threshold" | sudo tee --append /etc/init.d/cpu_gov.sh > /dev/null 2>&1
+			echo "echo $1 | sudo tee /sys/devices/system/cpu/cpufreq/$governor/up_threshold" | sudo tee --append /etc/init.d/cpu_gov > /dev/null 2>&1
 		else
 			echo "failed to chanage threshold. Not the correct governor."
 		fi
@@ -208,9 +208,14 @@ function uninstall_handler {
 			exit
 	    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 		fi
+		sudo update-rc.d cpu_gov remove
 		sudo rm /usr/local/bin/cpu.gov
-		sudo rm /etc/init.d/cpu_gov.sh
-		echo "CPU.GOV is now uninstalled"
+		sudo rm /etc/init.d/cpu_gov
+		if [[ $(sudo cat /etc/init.d/cpu_gov 3>&1 1>&2 2>&3) == *"No such file or directory"* ]] && [[ $(sudo cat /usr/local/bin/cpu.gov  3>&1 1>&2 2>&3) == *"No such file or directory"* ]]; then
+			echo "CPU.GOV is now uninstalled"
+			exit
+		fi
+		echo "Uninstall was not succesfull"
 		exit
 	fi
 }
--- installer/setup.sh
+++ installer/setup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-FILENAME="cpu.gov"
-
 
 if [[ $(cat /etc/init.d/cpu_gov.sh  3>&1 1>&2 2>&3) == *"No such file or directory"* ]]; then
-	touch /etc/init.d/cpu_gov.sh
-	echo -e "#!/bin/sh\n### BEGIN INIT INFO\n# Provides:          "$FILENAME"\n# Required-Start:    $local_fs $network $named $time $syslog\n# Required-Stop:     $local_fs $network $named $time $syslog\n# Default-Start:     2 3 4 5\n# Default-Stop:      0 1 6\n# Description:       script to automatically set cpu governors and ondemand threshold to desired value at boot\n### END INIT INFO " > /dev/null 2>&1
-	chmod +x /etc/init.d/cpu_gov.sh
-	../"$FILENAME"
-	insserv -d "$FILENAME"
-	update-rc.d cpu_gov.sh defaults
+	touch /etc/init.d/cpu_gov
+	echo -e '#!/bin/sh\n### BEGIN INIT INFO\n# Provides:          cpu.gov\n# Required-Start:    $local_fs $network $named $time $syslog\n# Required-Stop:     $local_fs $network $named $time $syslog\n# Default-Start:     2 3 4 5\n# Default-Stop:      0 1 6\n# Description:       script to automatically set cpu governors and ondemand threshold to desired value at boot\n### END INIT INFO\n ' > /etc/init.d/cpu_gov
+	chmod +x /etc/init.d/cpu_gov
+	insserv -d cpu_gov
+	update-rc.d cpu_gov defaults
 fi
 cp ./cpu.gov.sh /usr/local/bin/cpu.gov
-sudo chmod 755 /usr/local/bin/cpu.gov
-sudo chmod +x /usr/local/bin/cpu.gov
+chmod 755 /usr/local/bin/cpu.gov
+chmod +x /usr/local/bin/cpu.gov
+echo "Successfully installed CPU.gov"
